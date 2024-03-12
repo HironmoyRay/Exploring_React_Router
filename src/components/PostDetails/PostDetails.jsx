c86a0fe350dbb106668647adc016e2ebdff56277
hironmoy-ray <hpray35@gmail.com>
2024-03-13 00:41:13 +0600

    use Navigate Hook

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,15 +1,27 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const PostDetails = () => {
-    const post = useLoaderData();
-    const {id,title,body} = post;
-    return (
-        <div className="w-1/2 mx-auto">
-            <h3 className="text-2xl font-semibold">Id: {id}</h3>
-            <p className=" font-bold py-4">Title: {title}</p>
-            <p className="font-semibold p-4"><small>{body}</small></p>
-        </div>
-    );
+    const navigate = useNavigate();
+  const post = useLoaderData();
+  const { id, title, body } = post;
+  const goBackHandle = () => {
+    navigate(-1);
+  };
+  return (
+    <div className="w-1/2 mx-auto">
+      <h3 className="text-2xl font-semibold">Id: {id}</h3>
+      <p className=" font-bold py-4">Title: {title}</p>
+      <p className="font-semibold p-4">
+        <small>{body}</small>
+      </p>
+      <button
+        onClick={goBackHandle}
+        className="bg-violet-300 px-4 rounded-md mb-4"
+      >
+        Go Back
+      </button>
+    </div>
+  );
 };
 
 export default PostDetails;
--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -1,13 +1,19 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const UserDetails = () => {
+    const navigate = useNavigate();
   const myUser = useLoaderData();
   const { id, name, email } = myUser;
+
+  const goBackHandle = () => {
+    navigate(-1);
+  }
   return (
     <div>
       <h2>User Id: {id}</h2>
       <h1>User Name: {name}</h1>
       <p>Email: {email}</p>
+      <button onClick={goBackHandle} className="bg-violet-300 px-4 rounded-md m-4">Go Back</button>
     </div>
   );
 };
